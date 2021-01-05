66904f9a4f6d04e35c82a49c9a3c8ac2b752516d
arlet2020 <arlet_vazquez@hotmail.com>
2021-01-05 14:20:43 -0500

    More lotide exercises

--- takeUntil.js
+++ takeUntil.js
@@ -1,6 +1,26 @@
 const takeUntil = function (array, callback) {
-  // ...
-}
+
+  const newArray = []; // 1 // empry array
+  for (const x of array) { // looping entre parameters
+     //console.log(callback(x), x);
+    if (callback(x)) { // mi valor verdadero
+      break
+      //console.log(x);
+    } else {
+      newArray.push(x) 
+    }
+  }
+  //console.log(newArray); // each item in my array
+
+////
+
+ //array.forEach((item, index) => { // //posicion de mis elementos en mi array solo EXAMPLE
+ //  console.log(index, item);
+ // });
+  
+ return newArray // 2 
+};
+
 
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5]; // array
@@ -12,6 +32,7 @@ console.log('---');
 
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
+const results2 = takeUntil(data2, x => x === ','); /// call back desde la x hasta ' '
 console.log(results2);
+
 
